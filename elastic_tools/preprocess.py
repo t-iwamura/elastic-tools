07b0ac6503e08683fc7290f03750e3ed4b02193b
Taiki Iwamura <takki.0206@gmail.com>
2022-07-07 13:16:09 +0900

    Make each deformation directory in preprocess mode

--- elastic_tools/preprocess.py
+++ elastic_tools/preprocess.py
@@ -14,11 +14,11 @@ def make_deformed_structures(inputs_dir: str) -> None:
     poscar = Poscar.from_file(poscar_filename)
     deformed_structures = DeformedStructureSet(poscar.structure)
 
-    deformed_dir_path = Path(inputs_dir) / "deformed"
-    if not deformed_dir_path.exists():
-        deformed_dir_path.mkdir(parents=True)
-
     for i, structure in enumerate(deformed_structures):
-        new_poscar_path = deformed_dir_path / f"poscar-{str(i+1).zfill(3)}"
+        deformed_dir_path = Path(inputs_dir) / f"deform-{str(i+1).zfill(3)}"
+        if not deformed_dir_path.exists():
+            deformed_dir_path.mkdir(parents=True)
+
+        new_poscar_path = deformed_dir_path / "POSCAR"
         new_poscar = Poscar(structure)
         new_poscar.write_file(new_poscar_path)
