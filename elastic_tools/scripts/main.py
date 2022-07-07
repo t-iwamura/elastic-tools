d53729a773cd33450f9302d5aeb2fa28fb069e50
Taiki Iwamura <takki.0206@gmail.com>
2022-07-07 12:26:59 +0900

    Add logger to elastic_tools/scripts/main.py

--- elastic_tools/scripts/main.py
+++ elastic_tools/scripts/main.py
@@ -1,3 +1,5 @@
+import logging
+
 import click
 
 from elastic_tools.config import load_config
@@ -8,9 +10,14 @@ from elastic_tools.preprocess import make_deformed_structures
 @click.argument("config_file", nargs=1)
 def main(config_file):
     """Tools for elastic constants calculation"""
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info(" Load config file")
     config = load_config(config_file)
 
     if config.mode == "preprocess":
+        logging.info(" Start preprocess for elastic constants calculation")
+        logging.info(f"     inputs dir: {config.inputs_dir}")
+        logging.info(" Make deformed structures")
         make_deformed_structures(config.inputs_dir)
 
     if config.mode == "postprocess":
