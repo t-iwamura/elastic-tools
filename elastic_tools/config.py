2be0191abe45de5b5bf436992135323262d67922
Taiki Iwamura <takki.0206@gmail.com>
2022-07-07 11:06:56 +0900

    Add the package body

---
+++ elastic_tools/config.py
@@ -0,0 +1,24 @@
+import json
+from dataclasses import dataclass
+
+from dataclasses_json import dataclass_json
+
+
+@dataclass_json
+@dataclass
+class Config:
+    mode: str
+
+
+def load_config(path: str) -> Config:
+    """Load configs/*.json
+
+    Args:
+        path (str): path to configs/*.json
+
+    Returns:
+        Config: Elastic Constants calculation config dataclass
+    """
+    with open(path) as f:
+        config_dict = json.load(f)
+    return Config.from_dict(config_dict)  # type: ignore
---
+++ elastic_tools/scripts/main.py
@@ -0,0 +1,8 @@
+import click
+
+
+@click.command()
+@click.argument("config_file", nargs=1)
+def main(config_file):
+    """Tools for elastic constants calculation"""
+    pass
