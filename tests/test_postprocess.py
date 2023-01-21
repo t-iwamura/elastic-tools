b7dffeeecd0a3ac1901d4a0adb020a8aecbc5c86
Taiki Iwamura <takki.0206@gmail.com>
2023-01-21 19:08:25 +0900

    Fix tests/test_preprocess.py

--- tests/test_postprocess.py
+++ tests/test_postprocess.py
@@ -1,10 +1,18 @@
 import numpy as np
+from pymatgen.analysis.elasticity.elastic import ElasticTensor
 
-from elastic_tools.postprocess import calc_elastic_constants
+from elastic_tools.postprocess import read_calc_results
 
 
 def test_elastic_constants(al_dir_path):
-    stiffness = calc_elastic_constants(str(al_dir_path))
+    stress_list, strain_list, eq_stress = read_calc_results(calc_dir=str(al_dir_path))
+    et = ElasticTensor.from_independent_strains(
+        strains=strain_list,
+        stresses=stress_list,
+        eq_stress=eq_stress,
+        vasp=True,
+        tol=1e-4,
+    )
     expected_stiffness = np.array(
         [
             [104, 73, 73, 0, 0, 0],
@@ -15,4 +23,4 @@ def test_elastic_constants(al_dir_path):
             [0, 0, 0, 0, 0, 32],
         ]
     )
-    np.testing.assert_allclose(stiffness, expected_stiffness, rtol=0.3)
+    np.testing.assert_allclose(et.voigt, expected_stiffness, rtol=0.3)
