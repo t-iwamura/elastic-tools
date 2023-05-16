ae85a89f72a45e837f14c21c81812b9b68c737f8
Taiki Iwamura <takki.0206@gmail.com>
2023-05-16 17:22:31 +0900

    Bumped version number to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="elastic_tools",
-    version="0.1.0",
+    version="1.1.0",
     author="Taiki Iwamura",
     author_email="takki.0206@gmail.com",
     description=("Tools for elastic constants calculation"),
